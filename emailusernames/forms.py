0be0bb2b8a0fbe0764289aefdd5c204d48c40ec7
Лушковский Сергей <s.lushkovsky@gmail.com>
2016-09-04 18:19:35 +0300

    Fix for django UserChangeForm nesting
    'fields' is required (django == 1.9.7)
    Otherwise, raises: "django.core.exceptions.ImproperlyConfigured: Creating a ModelForm without either the 'fields' attribute or the 'exclude' attribute is prohibited; form EmailUserChangeForm needs updating."
    
    !IMPORTANT: Maybe this solution isn't backward compatible with older Django versions.

--- emailusernames/forms.py
+++ emailusernames/forms.py
@@ -103,6 +103,7 @@ class EmailUserChangeForm(UserChangeForm):
 
     class Meta:
         model = User
+        fields = ['email', 'password']
 
     def __init__(self, *args, **kwargs):
         super(EmailUserChangeForm, self).__init__(*args, **kwargs)
